acf5e8b4a655cc7ca92122d7e5bf1ba9918fe676
Ricardo Cardoso <ricardocardoso@MacBook-Air-de-Tamiris.local>
2016-09-28 01:20:42 -0300

    Implemented Person Class

--- ProjectMVVM.Entity/Person.cs
+++ ProjectMVVM.Entity/Person.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using SQLite;
+
 namespace ProjectMVVM.Entity
 {
+	[Table("Person")]
 	public class Person
 	{
 		public Person()
 		{
 		}
+
+		[Column("Id")]
+		[AutoIncrement, PrimaryKey]
+		public int Id { get; set; }
+
+		[Column("Nome")]
+		[MaxLength(150)]
+		public string Nome { get; set; }
+
+		[Column("Email")]
+		[MaxLength(100)]
+		public string Email { get; set; }
+
+		[Column("Endereco")]
+		[MaxLength(200)]
+		public string Endereco { get; set;}
+
 	}
 }
