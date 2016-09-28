a6ad7a9ec656ac71639542e50a17e7d81eab16da
Ricardo Cardoso <ricardocardoso@MacBook-Air-de-Tamiris.local>
2016-09-28 02:12:54 -0300

    Implement ViewModelBase

--- ProjectMVVM.Data/DataService/ISQLiteOperation.cs
+++ ProjectMVVM.Data/DataService/ISQLiteOperation.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using ProjectMVVM.Entity;
+
 namespace ProjectMVVM.Data
 {
 	public interface ISQLiteOperation
 	{
+		void Save(Person person);
+		void Remove(Person person);
+		void Update(Person person);
+		IList<Person> Select();
 	}
 }
---
+++ ProjectMVVM.Data/PersonDataService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectMVVM.Entity;
+using SQLite;
+
+namespace ProjectMVVM.Data
+{
+	public class PersonDataService : ISQLiteOperation
+	{
+		private SQLiteConnection _connection;
+
+		public PersonDataService(SQLiteConnection connection)
+		{
+			//_connection = DependencyService.Get<ISQLite>().GetConnection();
+			this._connection = connection;
+			_connection.CreateTable<Person>();
+		}
+
+		public void Remove(Person person)
+		{
+			_connection.Delete(person);
+		}
+
+		public void Save(Person person)
+		{
+			_connection.Insert(person);
+		}
+
+		public IList<Person> Select()
+		{
+			return _connection.Table<Person>().ToList();
+		}
+
+		public void Update(Person person)
+		{
+			_connection.Update(person);
+		}
+	}
+}
